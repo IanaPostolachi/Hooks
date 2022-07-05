7e78a65e4b8775406662e5ebfd0787af4b2a0bdd
Iana01 <304119@via.dk>
2022-07-05 16:51:20 +0200

    final hook version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  function increment()
-  {
-    setCount(prevCount => prevCount +1 )
-  }
+  // function increment() {
+  //   setCount((prevCount) => prevCount + 1);
+  // }
 
-  return <div className="App">
-    <button onClick={ increment}>+</button>
-    <h1>{count}</h1>
-    <button>-</button>
-  </div>;
+  // function decrement() {
+  //   setCount((prevCount) => prevCount - 1);
+  // }
+
+  return (
+    <div className="App">
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+    </div>
+  );
 };
 
 export default App;
